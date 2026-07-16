934dd268d5086225fe4a52253041b1f084929830
Vamsichowdhury <vamsichowdhury@gmail.com>
2026-07-16 21:58:30 +0530

    feat: Implement unflattenObject function to convert flattened objects into nested structures

--- coding-questions/js/solutions/1-Array-Object-Manipulation/medium/2-unflatten-object/sol.js
+++ coding-questions/js/solutions/1-Array-Object-Manipulation/medium/2-unflatten-object/sol.js
@@ -1,1 +1,29 @@
-//TODO
+/*
+// TODO
+Implement a function unflattenObject(obj) that converts a flattened object into a nested object.
+
+const obj = {
+  "user.name": "John",
+  "user.age": 25,
+  "user.address.city": "New York"
+};
+
+{
+  user: {
+    name: "John",
+    age: 25,
+    address: {
+      city: "New York"
+    }
+  }
+}
+
+*/
+
+function unflatten(obj) {
+  for (skey in obj) {
+    const keys = skey.split(".");
+    for (key of keys) {
+    }
+  }
+}
